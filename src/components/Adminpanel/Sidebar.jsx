e24311dd9158d5a75cdd9b1f6f6c33f7bf6a02f4
AnujaSreeni <127194605+AnujaSreeni@users.noreply.github.com>
2024-01-10 11:19:11 +0530

    first commit

---
+++ src/components/Adminpanel/Sidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <ul>
+        <a href='/home'><li>HOME</li></a>
+       </ul>
+        Registrations
+       <ul>
+        <a href='/c'><li>Category</li></a>
+        <a href='/s'><li>SubCategory</li></a>
+       </ul>
+       View
+       <ul>
+       <a href="/viewdetails"><li>Category View</li></a>
+       <a href="/sview"><li>Subcategory View</li></a>
+      
+      </ul>
+      
+    </div>
+  );
+};
+
+export default Sidebar;
